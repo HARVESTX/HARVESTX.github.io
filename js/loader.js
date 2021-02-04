c0e0773bf4f561ae92bf54de979124f769fedab6
m12watanabe1a <m12watanabe1a@users.noreply.github.com>
2021-02-04 16:29:58 +0000

    deploy: HarvestX/hx_web2@48707c53a20ec95c2e3569430c0b3f97a2e5f779

--- js/loader.js
+++ js/loader.js
@@ -1,7 +1,16 @@
 // loading画面
+document.addEventListener('DOMContentLoaded', () => {
+    document.addEventListener("mousewheel", preventEvent, { passive: false })
+    document.addEventListener("touchmove", preventEvent, { passive: false })
+})
 
 window.addEventListener('load', function(){
     const loading = document.getElementById('loader')
     loading.classList.add("loaded");
+    document.removeEventListener("mousewheel", preventEvent, { passive: false })
+    document.removeEventListener('touchmove', preventEvent, { passive: false })
 })
 
+function preventEvent(event) {
+    event.preventDefault();
+}
--- js/main.js
+++ js/main.js
@@ -15,7 +15,22 @@ menuBtn.onclick = function(){
   menuOpen = !menuOpen
 }
 
-
 function preventEvent(event) {
   event.preventDefault();
 }
+
+//ファーストビューの高さの設定
+const isSP = /iPhone|iPod|iPad|Android|Macintosh/i.test(navigator.userAgent) && 'ontouchend' in document
+ 
+// 端末の種類をもとにCSS変数を定義
+const setOuterHeight = () =>{
+  if(isSP){
+    console.log(isSP)
+    document.documentElement.style.setProperty(
+      '--outer-height',
+      `${window.outerHeight}px`
+    )
+  }
+}
+
+document.addEventListener('DOMContentLoaded', setOuterHeight)
